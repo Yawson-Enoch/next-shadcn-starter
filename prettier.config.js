5ff9d6431e622a8f45b06ac72540bead31176a96
Yawson-Enoch <enochgybex25@gmail.com>
2024-03-19 23:39:35 +0000

    chore: initial project setup

---
+++ prettier.config.js
@@ -0,0 +1,31 @@
+// @ts-check
+
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
+module.exports = {
+  singleQuote: true,
+  importOrder: [
+    '^(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^types$',
+    '^~/env(.*)$',
+    '^~/types/(.*)$',
+    '^~/config/(.*)$',
+    '^~/schemas/(.*)$',
+    '^~/lib/(.*)$',
+    '^~/store/(.*)$',
+    '^~/atoms/(.*)$',
+    '^~/context/(.*)$',
+    '^~/hooks/(.*)$',
+    '^~/components/ui/(.*)$',
+    '^~/components/(.*)$',
+    '^~/styles/(.*)$',
+    '^~/app/(.*)$',
+    '^~/server/(.*)$',
+    '^~/public/(.*)$',
+    '',
+    '^[.]',
+  ],
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+};
